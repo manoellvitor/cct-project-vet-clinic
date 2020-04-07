d9d5011128e98f9dc7376bc7abab217a600bbe52
Manoel <manoelvitorka@gmail.com>
2020-04-07 11:12:40 +0100

    Start with organization of the packages

---
+++ src/ie/cct/vetclinicca/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ie.cct.vetclinicca;
+
+/**
+ * @author Manoel Vitor Nascimento Lopes
+ * @college CCT - College
+ * @studentNumber 2019286
+ * @gitHub https://github.com/manoellvitor/vetClinicCA
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
