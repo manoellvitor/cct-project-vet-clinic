9a666e7dce0e8872508be0c6996a53a2ffe34799
Manoel <manoelvitorka@gmail.com>
2020-04-07 12:09:14 +0100

    Creatting the helper pakage, added the petNames.csv file & started the generateAnimal name Function

--- src/ie/cct/helper/Helper.java
+++ src/ie/cct/helper/Helper.java
@@ -1,5 +1,12 @@
 package ie.cct.helper;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author Manoel Vitor Nascimento Lopes
  * @college CCT - College
@@ -10,8 +17,22 @@ package ie.cct.helper;
 public class Helper {
 	
 	//	Function to generate the Animals names
-	public void generateAnimalName() {
-		
+	public static List<String[]> generateAnimalName() {
+		String file = "/home/manoel/eclipse-workspace/vetClinicCa/vetClinicCA/src/resources/petNames.csv";
+		List<String[]> names = new LinkedList<String[]>();
+		String name;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			while ((name = br.readLine()) != null) {
+				names.addAll(name);
+			 }
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR READING THE FILE, CHECK YOUR FILE: " + file);
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
